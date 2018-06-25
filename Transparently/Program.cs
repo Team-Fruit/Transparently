17a144815a6e63031de14a19e8da8d233fab5788
sjcl <fruit@bebehp.com>
2018-06-26 00:16:31 +0900

    refactor

--- Transparently/Program.cs
+++ Transparently/Program.cs
@@ -43,40 +43,36 @@ namespace Transparently {
         }
 
         static void Transparently(string path) {
-            try {
-                Color[,] pixelData;
-                int width, height;
-                ImageFormat format;
-                using (Bitmap tmpImg = new Bitmap(path)) {
-                    using (Bitmap img = new Bitmap(tmpImg)) {
-                        width = img.Width;
-                        height = img.Height;
-                        format = img.RawFormat;
-                        pixelData = new Color[img.Width, img.Height];
-                        for (int y = 0; y < height; y++) {
-                            for (int x = 0; x < width; x++) {
-                                Color color = img.GetPixel(x, y);
-                                if (color.R == 0 && color.G == 0 && color.B == 0) {
-                                    pixelData[x, y] = TransparentColor;
-                                } else {
-                                    pixelData[x, y] = color;
-                                }
+            Color[,] pixelData;
+            int width, height;
+            ImageFormat format;
+            using (Bitmap tmpImg = new Bitmap(path)) {
+                using (Bitmap img = new Bitmap(tmpImg)) {
+                    width = img.Width;
+                    height = img.Height;
+                    format = img.RawFormat;
+                    pixelData = new Color[img.Width, img.Height];
+                    for (int y = 0; y < height; y++) {
+                        for (int x = 0; x < width; x++) {
+                            Color color = img.GetPixel(x, y);
+                            if (color.R == 0 && color.G == 0 && color.B == 0) {
+                                pixelData[x, y] = TransparentColor;
+                            } else {
+                                pixelData[x, y] = color;
                             }
                         }
                     }
                 }
-                using (Bitmap saveImg = new Bitmap(width, height)) {
-                    for (int y = 0; y < height; y++) {
-                        for (int x = 0; x < width; x++) {
-                            saveImg.SetPixel(x, y, pixelData[x, y]);
-                        }
-                    }
-                    saveImg.Save(path, format);
-                }
-                Console.WriteLine("success!");
-            } catch (Exception e) {
-                Console.WriteLine(e.ToString());
             }
+            using (Bitmap saveImg = new Bitmap(width, height)) {
+                for (int y = 0; y < height; y++) {
+                    for (int x = 0; x < width; x++) {
+                        saveImg.SetPixel(x, y, pixelData[x, y]);
+                    }
+                }
+                saveImg.Save(path, format);
+            }
+            Console.WriteLine("success!");
         }
     }
 }
